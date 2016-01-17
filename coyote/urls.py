bcf899095c9cc195d44e575eebd5c20ae77bf2a8
Max Wang <maxwang051@gmail.com>
2016-01-17 05:44:41 -0600

    changed urls import settings

--- coyote/urls.py
+++ coyote/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url, patterns, settings
+from django.conf.urls import include, url, patterns
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
