43a67af717757e9c5594ecf90fc1b21ccd3006ca
Max Wang <maxwang051@gmail.com>
2016-01-17 01:10:59 -0600

    change settings

--- coyote/settings.py
+++ coyote/settings.py
@@ -56,9 +56,10 @@ MIDDLEWARE_CLASSES = [
 ]
 
 ROOT_URLCONF = 'coyote.urls'
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = '/../recipes/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
