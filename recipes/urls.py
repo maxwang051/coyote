fc638cd2f7f20f87421bce2524b34edd8f29ae47
Max Wang <maxwang051@gmail.com>
2016-01-16 20:33:26 -0600

    change twilio view

--- recipes/urls.py
+++ recipes/urls.py
@@ -10,8 +10,5 @@ urlpatterns = [
     url(r'^list/(?P<ingredients>.+?)/$', views.recipe_list, name='recipe_list'),
     url(r'^(?P<recipe_id>[0-9]+)/(?P<ingredients>.+?)/$', views.detail_view, name='detail'),
     url(r'^ingredients/', views.choose_ingredients, name='choose_ingredients'),
-    url(r'^message/$', 'recipes.views.sms', {
-        'message': 'Thanks for da SMS',
-        'to': '+18328593364',
-    }),
+    url(r'^message/$', 'recipes.views.sms'),
 ]
