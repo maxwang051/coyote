b495839d9375623c4189b4fb45a6d31e60e0016b
Max Wang <maxwang051@gmail.com>
2016-01-17 01:00:14 -0600

    changed settings

--- coyote/settings.py
+++ coyote/settings.py
@@ -56,10 +56,13 @@ MIDDLEWARE_CLASSES = [
 ]
 
 ROOT_URLCONF = 'coyote.urls'
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = "/static/"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
