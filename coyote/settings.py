dcad3d9e022e32804304db9ddf6fb3248f53ab69
Max Wang <maxwang051@gmail.com>
2016-01-16 18:55:10 -0600

    trying to send sms

--- coyote/settings.py
+++ coyote/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes',
+    'django_twilio',
 ]
 
 MIDDLEWARE_CLASSES = [
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, patterns, include
 from django.contrib import admin
+import django_twilio
 
 from recipes import views
 
@@ -9,5 +10,7 @@ urlpatterns = [
     url(r'^list/(?P<ingredients>.+?)/$', views.recipe_list, name='recipe_list'),
     url(r'^(?P<recipe_id>[0-9]+)/(?P<ingredients>.+?)/$', views.detail_view, name='detail'),
     url(r'^ingredients/', views.choose_ingredients, name='choose_ingredients'),
-    url(r'^sms/$', views.sms),
+    url(r'^message/$', 'django_twilio.views.message', {
+        'message': 'Thanks for da SMS',
+    }),
 ]
