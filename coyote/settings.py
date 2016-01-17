af51200578718ef6e54ddf4b76ed7761c98d7bef
Max Wang <maxwang051@gmail.com>
2016-01-17 05:36:00 -0600

    changed settings

--- coyote/settings.py
+++ coyote/settings.py
@@ -58,9 +58,7 @@ MIDDLEWARE_CLASSES = [
 ROOT_URLCONF = 'coyote.urls'
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT,'static/'),
-)
+STATICFILES_DIRS = ()
 
 
 TEMPLATES = [
